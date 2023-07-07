168ad56130d701b3cb54469b7074e4912217b0c6
Christian Crespo <110556053+ChristianC81@users.noreply.github.com>
2023-07-07 16:24:37 -0500

    Initial commit

---
+++ src/main/java/com/actividadtap/crud/repositories/IProductDAO.java
@@ -0,0 +1,14 @@
+package com.actividadtap.crud.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.actividadtap.crud.models.ProductDTO;
+
+
+
+@Repository
+public interface IProductDAO extends MongoRepository<ProductDTO, String> {
+	
+	
+}
